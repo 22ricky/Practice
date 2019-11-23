42ce9706c33210f684bd24ec509ac0a7f16a6b21
zhouguangyao <wyprivatemail@163.com>
2019-11-23 23:15:42 +0800

    Flutter: Container Widget

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -11,18 +11,41 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter')
         ),
         body: Center(
-          child: Text(
-            'Hello World,this is a test flutter demo and welcome to here.',
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 25.0,
-              color: Color.fromARGB(255, 24, 144, 255),
-              decoration: TextDecoration.underline,
-              decorationStyle: TextDecorationStyle.dashed,
+          child: Container(
+            child: Text(
+              'Hello Fultter',
+              style: TextStyle(
+                fontSize: 40.0,
+              ),
+            ),
+            alignment: Alignment.topLeft,
+            width: 500.0,
+            height: 400.0,
+            // color: Colors.lightBlue,
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 0.0),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+              ),
+              border: Border.all(
+                width: 5.0,
+                color: Colors.red
+              )
             ),
           ),
+          // child: Text(
+          //   'Hello World,this is a test flutter demo and welcome to here.',
+          //   textAlign: TextAlign.center,
+          //   maxLines: 1,
+          //   overflow: TextOverflow.ellipsis,
+          //   style: TextStyle(
+          //     fontSize: 25.0,
+          //     color: Color.fromARGB(255, 24, 144, 255),
+          //     decoration: TextDecoration.underline,
+          //     decorationStyle: TextDecorationStyle.dashed,
+          //   ),
+          // ),
         ),
       ),
     );
